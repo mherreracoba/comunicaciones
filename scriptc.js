05e9212ed99db5a41e5c5b7ace621b24059b746d
Miroslava Herrera <miroslava.herrera@gmail.com>
2019-09-26 17:27:52 -0500

    mod

--- scriptc.js
+++ scriptc.js
@@ -1,9 +1,34 @@
+const app= document.getElementById('root') //TODA LA ETIQUETA ROOT DEL CSS
+const logo= document.createElement('img') //HIJITOS POR PELICULA
+const container= document.createElement('div')
+container.setAttribute('class', 'container')
+logo.src='logo.jpg'
+
+// COINATRIBUTO AL DIV CLASE Y CONTANINER
+app.appendChild(logo)
+app.appendChild(container)
+
 var request = new XMLHttpRequest()
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
 request.onload=function() {
 var data = JSON.parse(this.response)
+//.forEach(movie=> {
+//console.log(movie.title)
+//})
 data.forEach(movie=> {
-console.log(movie.title)
-})
+    const card =document.createElement('div')
+    card.setAttribute('class', 'card')
+    const h1= document.createElement('h1')
+    h1.textContent=movie.title
+    const p=document.createElement('p')
+    movie.description=movie.description.substring(0, 300) // Limit to 300 chars
+    p.textContent=`${movie.description}...`
+    container.appendChild(card)
+    card.appendChild(h1)
+    card.appendChild(p)
+    })
+
 }
 request.send()
+
+
